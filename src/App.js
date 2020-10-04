98baaf969824dc94e17ad72b1680c29b0d400278
jedfarm <jfand72@gmail.com>
2020-10-04 13:12:52 -0400

    Changed class by function and useState

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons:[
-      {name:'Jesus', age: 48},
-      {name: 'Elisa', age: 9},
-      {name: 'Camila', age: 39}
-    ]
-  }
+const App = props => {
+  
+  const [personState, setPersonsState] = useState({
+      persons:[
+        {name:'Jesus', age: 48},
+        {name: 'Elisa', age: 9},
+        {name: 'Camila', age: 39}
+      ], 
+      otherState: 'some other value'
+    });
 
-  switchNameHandler = () => {
-    //console.log('Was clicked!');
-    // DON'T DO THIS!!! this.state.persons[0].name = 'Jesus E.';
-    this.setState({persons:[
-      {name:'Jesus E.', age: 48},
-      {name: 'Elisa', age: 9},
-      {name: 'Camila', age: 39}
-    ]
-  })
-
-  }
-
-  render() {
+    const switchNameHandler = () => {
+      //console.log('Was clicked!');
+      // DON'T DO THIS!!! this.state.persons[0].name = 'Jesus E.';
+      setPersonsState({persons:[
+        {name:'Jesus E.', age: 48},
+        {name: 'Elisa', age: 9},
+        {name: 'Camila', age: 39}
+      ]
+    });
+    };
+  
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age={this.state.persons[0].age}> My hobbies: Chess </Person>
-        <Person name = {this.state.persons[1].name} age={this.state.persons[1].age}> </Person>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name = {personState.persons[0].name} age={personState.persons[0].age}> My hobbies: Chess </Person>
+        <Person name = {personState.persons[1].name} age={personState.persons[1].age}> </Person>
       </div>
     );
     
    //return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi, I\'m a React App!!!'));
   }
-}
+
 
 export default App;
+
+
+
