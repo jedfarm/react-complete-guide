916136ea19bf2c1d2e6f886584a24c50f6bf3124
jedfarm <jfand72@gmail.com>
2020-10-03 21:27:03 -0400

    coded basic examples:  state,  props and handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons:[
+      {name:'Jesus', age: 48},
+      {name: 'Elisa', age: 9},
+      {name: 'Camila', age: 39}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    // DON'T DO THIS!!! this.state.persons[0].name = 'Jesus E.';
+    this.setState({persons:[
+      {name:'Jesus E.', age: 48},
+      {name: 'Elisa', age: 9},
+      {name: 'Camila', age: 39}
+    ]
+  })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name = {this.state.persons[0].name} age={this.state.persons[0].age}> My hobbies: Chess </Person>
+        <Person name = {this.state.persons[1].name} age={this.state.persons[1].age}> </Person>
+      </div>
+    );
+    
+   //return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi, I\'m a React App!!!'));
+  }
 }
 
 export default App;
